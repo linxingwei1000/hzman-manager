d65352dbcb759c3eb2ab21f33c7e5b5d02c9bdde
linxingwei <linxingwei@linxingweideMacBook-Air.local>
2020-07-11 17:48:39 +0800

    edit

--- hzman-server/src/main/resources/hzm.sql
+++ hzman-server/src/main/resources/hzm.sql
@@ -1,16 +1,9 @@
-create table hzm.hzm_item(
-  id int(8) primary key auto_increment,
-  asin varchar(128) not null default '' comment 'ASIN',
-  ctime bigint(20) default 0 not null comment '创建时间',
-  utime bigint(20) default 0 not null comment '更新时间')ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 drop table hzm.hzm_item;
 CREATE TABLE hzm.hzm_item (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `asin` varchar(128) not null default '' comment 'ASIN',
-  `ctime` bigint(20) default 0 not null comment '创建时间',
-  `utime` bigint(20) default 0 not null comment '更新时间',
+  `ctime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `utime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
   KEY `idx_asin` (`asin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品表';
