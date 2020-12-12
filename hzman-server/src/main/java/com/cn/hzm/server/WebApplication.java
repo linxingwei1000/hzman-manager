05da614974a46cac657b5d79af81faac7eea3a71
yuyang04 <yuyang04@corp.netease.com>
2020-12-12 16:07:13 +0800

    跨域拦截器

--- hzman-server/src/main/java/com/cn/hzm/server/WebApplication.java
+++ hzman-server/src/main/java/com/cn/hzm/server/WebApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableCaching
 @EnableScheduling
@@ -15,4 +19,17 @@ public class WebApplication {
         new SpringApplicationBuilder(WebApplication.class).web(WebApplicationType.SERVLET).build().run(args);
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
 }
